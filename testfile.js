caf108b14dc66830a9ce454d593094a74cdc6b2f
Ash Gowriluk <ashleerluk@gmail.com>
2021-01-13 14:13:16 +0100

    testfile

---
+++ testfile.js
@@ -0,0 +1,24 @@
+var x;
+var xx;
+x = 626;
+
+function timesTwo(n) {
+    return n * n;
+}
+xx = timesTwo(x);
+
+var numbers;
+
+numbers = [x, xx];
+
+var newNumbers = numbers.length;
+for (var i = 0; i < newNumbers; i++);
+{
+    console.log(numbers[i]);
+}
+
+var newObj = new Object();
+
+newObj.color;
+
+console.log(numbers);
