19f6f4101aa262e59ddef00bdb2733c164088ae3
Ash Gowriluk <ashleerluk@gmail.com>
2021-01-15 15:50:32 +0100

    3:50pm

--- newticker/script.js
+++ newticker/script.js
@@ -1,15 +1,49 @@
 var headlines = document.getElementById("headlines");
 var left = headlines.offsetLeft;
-console.log(left);
+var links = document.getElementsByTagName("a");
+var requestId;
 
-moveHeadlines();
+/*function moveHeadlines() {
+    left--;
+    if (left < -links[0].offsetWidth) {
+        left += links[0].offsetWidth;
+        headlines.appendChild(links[0]);
+    }
+    headlines.style.left = left + "px";
+    requestAnimationFrame(moveHeadlines);
+}
+moveHeadlines();*/
 
 function moveHeadlines() {
     left--;
-    if (left < headlines.firstChild.offsetWidth) {
-        headlines.style.left = "200px";
-        requestAnimationFrame(moveHeadlines);
+    if (left < -links[0].offsetWidth) {
+        left += links[0].offsetWidth;
+        headlines.appendChild(links[0]);
     }
-    console.log(moveHeadlines);
+    headlines.style.left = left + "px";
+    requestAnimationFrame(moveHeadlines);
 }
-requestAnimationFrame(moveHeadlines);
+moveHeadlines();
+
+for (var i = 0; i < links.length; i++) {
+    //console.log('links[i]: ',links[i]);
+    links[i].addEventListener("mouseenter", function (e) {
+        console.log("e.target mouse enter:", e.target);
+        //e target refers to the clicked element
+        e.target.style.font = "hotpink";
+        cancelAnimationFrame(requestId);
+    });
+    links[i].addEventListener("mouseleave", function (e) {
+        console.log("e.target mouse leave: ", e.target);
+        headlines.style.left = left + "px";
+        requestAnimationFrame(moveHeadlines);
+    });
+}
+
+function checkHeadlines() {
+    requestId = requestAnimationFrame(moveHeadlines);
+    return requestId;
+    //console.log("requestId: ", requestId);
+}
+
+checkHeadlines();
