36958e90c04175057c549cbfa0eaae0380d088c3
Ash Gowriluk <ashleerluk@gmail.com>
2021-01-18 21:11:14 +0100

    9:11pm Jan18

--- placing/script.js
+++ placing/script.js
@@ -1,11 +1,16 @@
-var sideNav = document.getElementById("side-nav");
+var sideNav = document.getElementsByClassName("side-nav");
 var menuButton = document.getElementById("menu");
 var x = document.getElementById("x");
+var overlayNew = document.getElementsByClassName("overlayNew");
 
 menuButton.addEventListener("click", function () {
-    console.log("i hear you");
-    sideNav.classList.add("on");
+    //console.log("i am listening");
+    sideNav[0].classList.add("on");
+    overlayNew[0].classList.remove("invisible");
 });
+
 x.addEventListener("click", function () {
-    sideNav.classList.remove("on");
+    //console.log("i hear you");
+    sideNav[0].classList.remove("on");
+    overlayNew[0].classList.add("invisible");
 });
