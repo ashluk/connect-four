fb0621ff165837ebb73d3362ea8e2a89662214d9
Ash Gowriluk <ashleerluk@gmail.com>
2021-01-25 09:56:10 +0100

    9:55am Jan.25

--- WEEK-THREE/connect4me/script.js
+++ WEEK-THREE/connect4me/script.js
@@ -1,4 +1,5 @@
 var currentPlayer = "player1";
+var winner = $(".red");
 var slotNumbers = $(".slot");
 var diags = [
     [0, 7, 14, 21],
@@ -29,11 +30,7 @@ var diags = [
 
 $(".column").on("click", function (e) {
     var col = $(e.currentTarget);
-    //console.log("e:", e);  //logging e will show all the available properties on the object
-    //console.log("column", col);
-    var slotsInCol = col.children(); //.children is a method available to us in JQ - allows us to select all the children of a specific elemnent
-    //console.log("slotsInCol", slotsInCol);
-    //reverse loop -- but why? -- because we want to start checking from the bottom --thats where the tokens are dropped
+    var slotsInCol = col.children();
     for (var i = slotsInCol.length - 1; i >= 0; i--) {
         //console.log("slotsInCol:", slotsInCol.eq(i));
         //check that the slot is free
@@ -42,52 +39,97 @@ $(".column").on("click", function (e) {
             !slotsInCol.eq(i).hasClass("player1") &&
             !slotsInCol.eq(i).hasClass("player2")
         ) {
-            //console.log("this slot must be free, add current player");
-            slotsInCol.eq(i).addClass(currentPlayer); //currentplayer is pointing to the string player1
+            slotsInCol.eq(i).addClass(currentPlayer);
             break;
         }
     }
-    //console.log("i:", i);
     if (i === -1) {
         return;
     }
 
     var slotsInRow = $(".row" + i);
     //once we have our slotsinrow we use this to check if there is a victory in a row, based on where the chip drops we check which row we need to check
-    console.log("this is my row", slotsInRow); //this should bring back the row number (i.e row3)
 
     if (checkForVictory(slotsInCol)) {
-        console.log("column victory!");
+        youWon(currentPlayer);
     } else if (checkForVictory(slotsInRow)) {
         console.log("row victory!");
         youWon(currentPlayer);
     } else if (checkForDiagonalVictory(slotNumbers)) {
-        console.log("diagonal victory!");
+        youWon(currentPlayer);
     }
 
     switchPlayer(); //this finishes the loop and switches the player by calling our switchplayer function
 });
 
 function youWon(player) {
-    console.log("player:", player);
-    $(".victory").animate({
-        display: "toggle",
-        width: "+=400px",
-        height: "+=400px",
+    if (player === "player1") {
+        winner = $(".red");
+    } else {
+        winner = $(".yellow");
+    }
+
+    var blink = setInterval(function () {
+        if (winner.css("visibility") == "hidden") {
+            winner.css("visibility", "visible");
+            winner.animate({ fontSize: "35em" }, "slow", "swing");
+
+            $(".connect").animate(
+                { rotation: 90 },
+                {
+                    duration: 500,
+                    step: function (now) {
+                        $(this).css({
+                            transform: "rotate(" + now + "deg)",
+                        });
+                    },
+                }
+            );
+        } else {
+            winner.css("visibility", "hidden");
+        }
+    }, 300);
+    setTimeout(function () {
+        winner.animate({ fontSize: "80px" }, "slow", "swing");
+        winner.css("visibility", "hidden");
+        $(".connect").animate(
+            { rotation: 0 },
+            {
+                duration: 500,
+                step: function (now) {
+                    $(this).css({
+                        transform: "rotate(" + now + "deg)",
+                    });
+                },
+            }
+        );
+
+        clearInterval(blink);
+    }, 3000);
+
+    $(".reset").on("click", function () {
+        $(".slot").removeClass("player1");
+        $(".slot").removeClass("player2");
+        clearInterval(blink);
+
+        $(".red").css("visibility", "hidden");
+        $(".yellow").css("visibility", "hidden");
     });
 }
 
-$(".reset").on("click", function () {
-    $(".slot").removeClass("player1");
-    $(".slot").removeClass("player2");
+$(".connect").on("click", function () {
+    $(".column").addClass(currentPlayer);
+    youWon(currentPlayer);
+
+    $(".reset").on("click", function () {
+        $(".column").removeClass("player1");
+        $(".column").removeClass("player2");
+    });
 });
 
 function checkForDiagonalVictory(allSlots) {
-    //console.log("all:", allSlots);
     for (var i = 0; i < diags.length; i++) {
-        //console.log(("i", diags[i]));
         var count = 0;
-        //console.log("************");
 
         for (var j = 0; j < diags[i].length; j++) {
             var index = diags[i][j];
@@ -102,16 +144,10 @@ function checkForDiagonalVictory(allSlots) {
             //console.log("array within;", diags[i][j]); //this loops over the arrays within the array
         }
     }
-    //console.log("checking for diag", diag);
 }
 function checkForVictory(slots) {
-    //console.log("checking for victory with these:", slots);
     var count = 0;
     for (var i = 0; i < slots.length; i++) {
-        /*console.log(
-            "slots.eq(i).hasClass():",
-            slots.eq(i).hasClass(currentPlayer)
-        );*/
         if (slots.eq(i).hasClass(currentPlayer)) {
             count++;
             if (count === 4) {
@@ -122,7 +158,6 @@ function checkForVictory(slots) {
         }
     }
 }
-
 function switchPlayer() {
     if (currentPlayer === "player1") {
         currentPlayer = "player2";
