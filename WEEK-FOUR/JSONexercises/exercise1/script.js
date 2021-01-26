b50be4ecaaa0753e96a81b9239ba40ce61b4e06d
Ash Gowriluk <ashleerluk@gmail.com>
2021-01-26 18:19:11 +0100

    6:19

--- WEEK-FOUR/JSONexercises/exercise1/script.js
+++ WEEK-FOUR/JSONexercises/exercise1/script.js
@@ -1,21 +1,28 @@
 var inputField = $("input");
 
 //1.make button click event
-$("button").mousedown(function () {});
-
-//2. listen to user input
-
-inputField.on("input", function () {
+$("button").on("click", function () {
     var userInput = inputField.val();
     var jsonObj = JSON.parse(userInput);
 
     console.log("input:", jsonObj);
+
+    try {
+        JSON.parse({ name: "ash" });
+    } catch (error) {
+        console.log("error: ", error);
+    }
 });
+//2. listen to user input
+
+/*inputField.on("input", function () {
+    var userInput = inputField.val();
+    var jsonObj = JSON.parse(userInput);
+
+    */
 
 //3.try catch for if userinput is not JSON
-try {
-    JSON.parse({ name: "ivana" });
-} catch (error) {
-    console.log("error: ", error);
-}
+
 //4.create popup
+
+// TEST JSON      {"result":true, "count":42}
