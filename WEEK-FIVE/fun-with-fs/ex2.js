f18bfa99181b8cd116e9e43deb72647f251580e2
Ash Gowriluk <ashleerluk@gmail.com>
2021-02-03 18:45:54 +0100

    6:45pm Feb3

--- WEEK-FIVE/fun-with-fs/ex2.js
+++ WEEK-FIVE/fun-with-fs/ex2.js
@@ -1,28 +1,35 @@
 const fs = require("fs");
 
-//const sizeMap =
-mapSizes(__dirname + "/files");
+const sizeMap = mapSizes(__dirname + "/files");
+fs.writeFileSync("files.json", JSON.stringify(sizeMap, null, 4));
 
 function mapSizes(path) {
     const items = fs.readdirSync(path, { withFileTypes: true });
-    const obj = {};
-    const actualObj = Object.create(obj);
-    //console.log("items", items);
+    const actualObj = {};
     items.forEach((item) => {
         if (item.isFile() === true) {
-            //console.log("item name", item.name);
-            actualObj.item.name = size;
-            // Object.defineProperty(actualObj) = item.name;
-            const size = fs.statSync(path + "/" + item.name);
+            //const size = fs.statSync(path + "/" + item.name);
+            const newSize = getSize(path + "/" + item.name);
+            actualObj[item.name] = newSize;
 
-            actualObj.value = size;
-            console.log("my object", actualObj);
+            //actualObj.value = newSize;
+            //console.log("my object", actualObj);
         } else {
-            console.log("directory name", item.name);
-            mapSizes(path + "/" + item.name);
+            //console.log("directory name", item.name);
+            //console.log("directory name", item.name);
+            const directory = mapSizes(path + "/" + item.name);
+            actualObj[item.name] = directory;
         }
     });
+    console.log(actualObj);
+    return actualObj;
 }
 
-const stats = fs.statSync(__filename);
-console.log("this is the size", stats.size);
+function getSize(name) {
+    var stats = fs.statSync(name);
+    var fileSize = stats.size;
+    return fileSize;
+}
+
+//const stats = fs.statSync(__filename);
+//console.log("this is the size", stats.size);
